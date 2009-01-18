d6a3cb78a3e1c78dba3469341d36ed9ef83de886
madx <root@yapok.org>
2009-01-18 22:51:27 +0100

    enhancing the module loading

--- lib/solylace.rb
+++ lib/solylace.rb
@@ -1,6 +1,15 @@
+# Require vendor librairies
+vndpath = File.join(File.dirname(__FILE__), 'vendor')
+$:.unshift vndpath unless $:.member?(vndpath)
+require 'metash/lib/metash'
+
 module Solylace
 end
 
-Dir[ File.join( File.dirname(__FILE__), 'solylace', '*.rb' ) ].each do |file|
-  require file
+# Require Solylace modules
+slpath = File.join(File.dirname(__FILE__), 'solylace')
+$:.unshift slpath unless $:.member?(slpath)
+
+%w(binder buffer command  selection).each do |mod|
+  require mod
 end
